16877ea9728c65be21789a437139ac03b9c2c0bf
Ben Brysiuk <benbrysiuk@gmail.com>
2021-02-20 22:50:52 -0800

    First Commit

---
+++ js/pageLoad.js
@@ -0,0 +1,27 @@
+﻿
+(function ($){
+    
+    $.fn.pageLoad = function (){
+
+        var slides = this.children('ol').children('li');
+        var pageIndex = ["Home", "BAE", "BAD", "BAA", "About"];
+
+        var loadPage = function (e) {
+            var $this = $(this);
+            var index = slides.index($this);
+            if (index == -1) {
+                $("#Homepage").load('Home.html body');
+                return;
+            };
+
+        }
+
+        slides.on('click.pageLoad', loadPage());
+
+
+
+     
+   
+
+    }
+}(jQuery));
